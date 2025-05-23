e01a81b4a9c2d4d62812e610d40cfc51b8685418
dell <dell@DESKTOP-SGR76FB>
2025-05-23 19:12:59 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using CrudAppUsingADO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudAppUsingADO.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            EmployeeDBContext db = new EmployeeDBContext();
+            List<Employee> obj = db.GetEmployees();
+            return View(obj);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            return View();
+        }
+    }
+}
